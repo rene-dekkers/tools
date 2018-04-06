b313ee493040367afc38fa2bb9322b2d58bd5bb3
René Dekkers <rene@dekkers.biz>
2018-04-06 09:14:45 +0200

    first commit

---
+++ ansible/cleanup-templating-delimiters.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# René Dekkers - 2018
+# Cleanup delimiters such as {{ }} or {% %} in a ansible yaml file
+if [ $# -ne 1 ]; then
+        echo "Usage: $0 <yml_file>"
+        exit 2
+fi
+
+YMLFILE=$1
+
+sed -i -E "s/when: ?\"\{\{ ?/when: \"/g" $YMLFILE
+sed -i -E "s/when: ?\"(.*) }}\"/when: \"\1\"/g" $YMLFILE
+
+sed -i -E "s/when: ?\"\{% ?/when: \"/g" $YMLFILE
+sed -i -E "s/when: ?\"(.*) %}\"/when: \"\1\"/g" $YMLFILE
